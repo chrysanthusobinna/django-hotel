c3a638abd671679ca8ff4ea844c528ed66a067b3
chrysanthusobinna <chrysanthusobinna@gmail.com>
2025-04-05 11:49:01 +0100

    Set up project environment and create core Django apps

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["SECRET_KEY"] = "django-insecure-*0yzd*td&ky@t#!ptvzp32u*h&@zs#0(f$bx%t!181)1_=de8k"
+os.environ["DEBUG"] = "True"
+os.environ["STRIPE_SECRET_KEY"] = "your-stripe-key"
+os.environ["STRIPE_PUBLIC_KEY"] = "your-stripe-publishable-key"
