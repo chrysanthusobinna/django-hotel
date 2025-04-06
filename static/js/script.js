b044acdcdb17130e73058c68590448843397de5f
chrysanthusobinna <chrysanthusobinna@gmail.com>
2025-04-06 02:16:37 +0100

    Enhance navbar functionality: added auto-close on outside click for mobile, unified hover effect across devices

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,14 @@
 $('.carousel').carousel({ interval: 4000 });
+
+
+ // toggle to close nav
+    $(document).ready(function () {
+        $(document).click(function (event) {
+            var clickOver = $(event.target);
+            var _opened = $("#mainNavbar").hasClass("show");
+            if (_opened && !clickOver.closest('.navbar').length) {
+                $(".navbar-toggler").click();  
+            }
+        });
+    });
+ 
