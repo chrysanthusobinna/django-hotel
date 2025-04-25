855d6a7d56515f8122b054331fbb693d8283323b
chrysanthusobinna <chrysanthusobinna@gmail.com>
2025-04-25 06:53:29 +0100

    update booking list to show room name with category and TBC status

--- hotel_booking/context_processors.py
+++ hotel_booking/context_processors.py
@@ -6,4 +6,6 @@ def site_info(request):
         "SITE_EMAIL": settings.SITE_EMAIL,
         "SITE_PHONE": settings.SITE_PHONE,
         "SITE_ADDRESS": settings.SITE_ADDRESS,
+        "CURRENCY_SYMBOL": settings.CURRENCY_SYMBOL,
+        "CURRENCY_CODE": settings.CURRENCY_CODE,
     }
